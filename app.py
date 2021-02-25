b62542085b06b7f72d9fa18106de7f9104f70267
N-Naoto <nao9909.n@gmail.com>
2021-02-25 23:26:30 +0900

    HTML&CSSのサイトを6個繋げた

--- app.py
+++ app.py
@@ -13,13 +13,33 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/work')
+@app.route('/works')
 def work():
-    return render_template('work.html')
+    return render_template('works.html')
 
-@app.route('/work/html&css_1')
+@app.route('/works/html&css_1')
 def work_hc1():
     return render_template('works/html&css_1.html')
+
+@app.route('/works/html&css_2')
+def work_hc2():
+    return render_template('works/html&css_2.html')
+
+@app.route('/works/html&css_3')
+def work_hc3():
+    return render_template('works/html&css_3.html')
+
+@app.route('/works/html&css_4')
+def work_hc4():
+    return render_template('works/html&css_4.html')
+
+@app.route('/works/html&css_5')
+def work_hc5():
+    return render_template('works/html&css_5.html')
+
+@app.route('/works/html&css_6')
+def work_hc6():
+    return render_template('works/html&css_6.html')
 
 # @app.route('/contact')
 # def contact():
